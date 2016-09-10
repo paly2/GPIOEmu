34992e5499f69ce9a8f4e8a1293ab3257d549f54
palige <plangrognet@laposte.net>
2016-09-10 12:33:03 +0200

    Improve setup script

--- setup.py
+++ setup.py
@@ -3,17 +3,18 @@
 from distutils.core import setup, Extension
 from shutil import copytree, rmtree
 
-classifiers = ['Development Status :: In development',
+classifiers = ['Development Status :: 4 - Beta',
                'Operating System :: POSIX :: Linux',
-               'License :: Lesser GNU General Public License',
+               'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
                'Intended Audience :: Developers',
                'Programming Language :: Python :: 2.7',
                'Programming Language :: Python :: 3',
                'Topic :: Software Development',
-               'Topic :: Emulation']
+               'Topic :: Software Development :: Testing',
+               'Topic :: Education']
 
 setup(name             = 'GPIOEmu',
-      version          = '0.0.0',
+      version          = '0.1',
       author           = 'Pierre-Adrien Langrognet',
       author_email     = 'plangrognet@laposte.net',
       description      = 'A module simulating the RPi.GPIO module',
@@ -23,10 +24,8 @@ setup(name             = 'GPIOEmu',
       url              = 'https://github.com/paly2/GPIOEmu',
       classifiers      = classifiers,
       #packages         = ['GPIOEmu'],
-      ext_modules      = [Extension(name='GPIOEmu', sources=['src/py_gpio.c', 'src/GUI.c', 'src/common.c', 'src/constants.c', 'src/py_pwm.c', 'src/event_gpio.c'], libraries=["SDL2", "pthread"])])
+      ext_modules      = [Extension(name='GPIOEmu', sources=['src/py_gpio.c', 'src/GUI.c', 'src/common.c', 'src/constants.c', 'src/py_pwm.c', 'src/event_gpio.c'], libraries=["SDL2", "pthread"])],
+      data_files=[('/usr/share/GPIOEmu/images', ['src/images/GPIO_rev3.bmp', 'src/images/high.bmp', 'src/images/low.bmp', 'src/images/in.bmp', 'src/images/out.bmp', 'src/images/icon.bmp', 'src/images/icon.bmp', 'src/images/pull-down.bmp', 'src/images/pull-up.bmp', 'src/images/unknown_state.bmp', 'src/images/unset.bmp']),
+                  ('/usr/share/GPIOEmu/images/pwm', ['src/images/pwm/pwm.bmp', 'src/images/pwm/0.bmp', 'src/images/pwm/1.bmp', 'src/images/pwm/2.bmp', 'src/images/pwm/3.bmp', 'src/images/pwm/4.bmp', 'src/images/pwm/5.bmp', 'src/images/pwm/6.bmp', 'src/images/pwm/7.bmp', 'src/images/pwm/8.bmp', 'src/images/pwm/9.bmp', 'src/images/pwm/per_cent.bmp'])])
 
-# Copy images to /usr/share/GPIOEmu
-print("copying src/images/ -> /usr/share/GPIOEmu/images/")
-rmtree("/usr/share/GPIOEmu/images", ignore_errors=True) # Just in case the "images" directory already exist, making copytree to fail.
-copytree("src/images", "/usr/share/GPIOEmu/images")
 print("\nDone!")
