362e02ba07eb4023104d4b5d751b9cc183d836eb
palige <plangrognet@laposte.net>
2016-09-10 14:25:27 +0200

    setup.py: add depends file, change version

--- setup.py
+++ setup.py
@@ -14,18 +14,15 @@ classifiers = ['Development Status :: 4 - Beta',
                'Topic :: Education']
 
 setup(name             = 'GPIOEmu',
-      version          = '0.1',
+      version          = '0.0.2',
       author           = 'Pierre-Adrien Langrognet',
       author_email     = 'plangrognet@laposte.net',
       description      = 'A module simulating the RPi.GPIO module',
-      long_description = open('README.md').read(),
+      long_description = open('README').read(),
       license          = 'LGPL',
       keywords         = 'Raspberry Pi GPIO Emulator',
       url              = 'https://github.com/paly2/GPIOEmu',
       classifiers      = classifiers,
-      #packages         = ['GPIOEmu'],
-      ext_modules      = [Extension(name='GPIOEmu', sources=['src/py_gpio.c', 'src/GUI.c', 'src/common.c', 'src/constants.c', 'src/py_pwm.c', 'src/event_gpio.c'], libraries=["SDL2", "pthread"])],
+      ext_modules      = [Extension(name='GPIOEmu', sources=['src/py_gpio.c', 'src/GUI.c', 'src/common.c', 'src/constants.c', 'src/py_pwm.c', 'src/event_gpio.c'], libraries=["SDL2", "pthread"], depends=['src/common.h', 'constants.h', 'event_gpio.h', 'GUI.h', 'py_pwm.h'])],
       data_files=[('/usr/share/GPIOEmu/images', ['src/images/GPIO_rev3.bmp', 'src/images/high.bmp', 'src/images/low.bmp', 'src/images/in.bmp', 'src/images/out.bmp', 'src/images/icon.bmp', 'src/images/icon.bmp', 'src/images/pull-down.bmp', 'src/images/pull-up.bmp', 'src/images/unknown_state.bmp', 'src/images/unset.bmp']),
                   ('/usr/share/GPIOEmu/images/pwm', ['src/images/pwm/pwm.bmp', 'src/images/pwm/0.bmp', 'src/images/pwm/1.bmp', 'src/images/pwm/2.bmp', 'src/images/pwm/3.bmp', 'src/images/pwm/4.bmp', 'src/images/pwm/5.bmp', 'src/images/pwm/6.bmp', 'src/images/pwm/7.bmp', 'src/images/pwm/8.bmp', 'src/images/pwm/9.bmp', 'src/images/pwm/per_cent.bmp'])])
-
-print("\nDone!")
